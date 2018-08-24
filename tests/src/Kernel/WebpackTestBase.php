d0b5aadb0c8156be07b6d5c1e62811d3c0058b4c
Błażej Owczarczyk <blazeyy@gmail.com>
2018-08-25 00:47:59 +0200

    Updated phpunit.xml.dist, added WebpackTestBase and BundlerTest.

--- tests/src/Kernel/WebpackTestBase.php
+++ tests/src/Kernel/WebpackTestBase.php
@@ -2,6 +2,38 @@
 
 namespace Drupal\Tests\webpack\Kernel;
 
-abstract class WebpackTestBase {
+use Drupal\KernelTests\KernelTestBase;
+
+abstract class WebpackTestBase extends KernelTestBase {
+
+  protected static $modules = [ 'system', 'webpack', 'webpack_test_libs' ];
+
+  /**
+   * @var \Drupal\webpack\LibrariesInspectorInterface
+   */
+  protected $librariesInspector;
+
+  /**
+   * @var \Drupal\webpack\WebpackConfigBuilderInterface
+   */
+  protected $webpackConfigBuilder;
+
+  /**
+   * @var \Drupal\webpack\BundlerInterface
+   */
+  protected $bundler;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->librariesInspector = $this->container->get('webpack.libraries_inspector');
+    $this->webpackConfigBuilder= $this->container->get('webpack.config_builder');
+    $this->bundler = $this->container->get('webpack.bundler');
+
+    $this->installConfig('system');
+  }
 
 }
