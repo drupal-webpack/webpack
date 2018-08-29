cbcd08b89bb4b3730cf8938de88df43952cbd237
Błażej Owczarczyk <blazeyy@gmail.com>
2018-08-29 23:17:28 +0200

    Moved output building to a dedicated plugin.

--- src/Plugin/Webpack/ConfigProcessors/ConfigProcessorBase.php
+++ src/Plugin/Webpack/ConfigProcessors/ConfigProcessorBase.php
@@ -2,12 +2,64 @@
 
 namespace Drupal\webpack\Plugin\Webpack\ConfigProcessors;
 
+use Drupal\Core\File\FileSystemInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Plugin\PluginBase;
+use Drupal\webpack\BundlerInterface;
 use Drupal\webpack\Plugin\ConfigProcessorPluginInterface;
+use Drupal\webpack\WebpackConfigBuilderInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Base class for Config Processors.
  */
-abstract class ConfigProcessorBase implements ConfigProcessorPluginInterface {
+abstract class ConfigProcessorBase extends PluginBase implements ConfigProcessorPluginInterface, ContainerFactoryPluginInterface {
+
+  /**
+   * @var \Drupal\Core\File\FileSystemInterface
+   */
+  protected $fileSystem;
+
+  /**
+   * @var \Drupal\webpack\BundlerInterface
+   */
+  protected $bundler;
+
+  /**
+   * @var \Drupal\webpack\WebpackConfigBuilderInterface
+   */
+  protected $webpackConfigBuilder;
+
+  /**
+   * ConfigProcessorBase constructor.
+   *
+   * @param array $configuration
+   * @param $plugin_id
+   * @param $plugin_definition
+   * @param \Drupal\Core\File\FileSystemInterface $fileSystem
+   * @param \Drupal\webpack\BundlerInterface $bundler
+   * @param \Drupal\webpack\WebpackConfigBuilderInterface $webpackConfigBuilder
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FileSystemInterface $fileSystem, BundlerInterface $bundler, WebpackConfigBuilderInterface $webpackConfigBuilder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->fileSystem = $fileSystem;
+    $this->bundler = $bundler;
+    $this->webpackConfigBuilder = $webpackConfigBuilder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('file_system'),
+      $container->get('webpack.bundler'),
+      $container->get('webpack.config_builder')
+    );
+  }
 
   /**
    * Returns the path to node_modules folder.
@@ -31,4 +83,6 @@ abstract class ConfigProcessorBase implements ConfigProcessorPluginInterface {
 
 }
 
-class WebpackConfigNodeModulesNotFoundException extends \Exception { }
+class WebpackConfigNodeModulesNotFoundException extends \Exception {
+
+}
--- src/Plugin/Webpack/ConfigProcessors/Output.php
+++ src/Plugin/Webpack/ConfigProcessors/Output.php
@@ -3,6 +3,7 @@
 namespace Drupal\webpack\Plugin\Webpack\ConfigProcessors;
 
 use Drupal\webpack\Annotation\WebpackConfigProcessor;
+use Drupal\webpack\WebpackConfigNotValidException;
 
 /**
  * Add the
@@ -18,7 +19,14 @@ class Output extends ConfigProcessorBase {
    * {@inheritdoc}
    */
   public function processConfig(&$config, $context) {
-    // TODO: Move output building here from WebpackDrushCommands.
+    $outputDirPrepared = $this->webpackConfigBuilder->getOutputDir(TRUE);
+    if (!$outputDirPrepared) {
+      throw new WebpackConfigNotValidException('Output directory is not writable.');
+    }
+    $config['output'] = [
+      'filename' => '[name].bundle.js',
+      'path' => $this->fileSystem->realpath($outputDirPrepared),
+    ];
   }
 
 }
